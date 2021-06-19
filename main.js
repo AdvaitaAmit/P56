14ee9e884bdd11aa3781a70013ef4b4cab4597a9
AdvaitaAmit <85395773+AdvaitaAmit@users.noreply.github.com>
2021-06-19 17:28:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function setDate(){
+ const d = new Date(); var dat = d.getDate(); var mon = d.getMonth(); var year = d.getFullYear(); document.getElementById("demo").innerHTML = dat+"/" + mon+ "/"+ year;
+   
+}
+
+   
